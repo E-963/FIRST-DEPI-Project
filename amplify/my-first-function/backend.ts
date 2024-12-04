d83826543fe752204f3493e5630776298a5b7894
E-963 <enanysama10@gmail.com>
2024-12-04 12:09:23 +0200

    Amplify AWS

---
+++ amplify/my-first-function/backend.ts
@@ -0,0 +1,10 @@
+import { myFirstFunction } from './my-first-function/resource';
+
+defineBackend({
+  ...
+  myFirstFunction,
+});
+
+function defineBackend(arg0: any) {
+    throw new Error('Function not implemented.');
+}
